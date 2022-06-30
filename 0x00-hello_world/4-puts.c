810122937ff9cc5ea6c15e67696e199689eff088
enanerose <enanerose97@gmail.com>
2022-06-30 14:14:36 -0700

    cprogramming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * C program that prints exactly
- * Programming is like building a multilingual puzzle,
- * followed by a new line.
+ * main - print the string in the put function
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 
-int main (void)
+int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-}
+	}
