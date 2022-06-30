0bc31d3452b239f5cdffc3fa43ba7e056ecb40dc
enanerose <enanerose97@gmail.com>
2022-06-30 13:55:38 -0700

    cprogramming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,14 @@
 /**
  * main prints the string in the put function
  *
- * this program prints programming is like buiding multilingual
- *
+ * Description: using the main function
+ * 
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
-int main(void)
-{
+   int main(void)
+  {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
+  }
