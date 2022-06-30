038cd28b1dd4c0477e75ac5546d4ea80e7fde7b0
enanerose <enanerose97@gmail.com>
2022-06-30 14:58:48 -0700

    cprogramming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,6 @@
 
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of a int : %Id byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
