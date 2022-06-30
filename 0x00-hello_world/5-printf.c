ee8ed664a269c59c51132c2cb895839a80acc655
enanerose <enanerose97@gmail.com>
2022-06-30 11:56:47 -0700

    c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main prints the string in the put function
+ *
+ * this program prints programming is like buiding multilingual
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
