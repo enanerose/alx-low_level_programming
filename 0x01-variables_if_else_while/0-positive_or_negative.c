4c50671f4b335506642c8884c168c568851f2cb9
enanerose <enanerose97@gmail.com>
2022-07-02 11:18:24 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,50 @@
-#include <stdlib.h>
+nclude <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
 /**
- * main - program entry point
- * Description : A program to determine wether an int is +ve, -ve or 0
- * Return : 0 indicates success
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/*your code goes there*/
-	if (n > 0)
-		printf(%d is positive\n", n);
-	if (n == 0)
-		printf(%d is zero\n", n);
-	if (n < 0)
-		printf(%d is negative\n", n);
-	return (0);
+
+int n;
+
+
+
+srand(time(0));
+
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+
+printf("%d is positive\n", n);
+
+
+
+if (n == 0)
+
+printf("%d is zero\n", n);
+
+
+
+if (n < 0)
+
+printf("%d is negative\n", n);
+
+
+
+return (0);
+
 }
