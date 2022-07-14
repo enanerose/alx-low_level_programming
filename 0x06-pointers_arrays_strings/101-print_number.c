5816edda40256e9a5892e486a8b4002de9799044
enanerose <enanerose97@gmail.com>
2022-07-14 12:41:42 -0700

    another commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,9 +12,7 @@
 void print_number(int n)
 
 {
-
 	unsigned int tens, dig, pos = n;
-
 	double temp_tens = 1;
 
 
@@ -29,14 +27,10 @@ void print_number(int n)
 		if (n < 0)
 
 		{
-
 			pos = n * -1;
-
 			_putchar('-');
 
 		}
-
-
 
 		while (temp_tens <= pos)
 
@@ -44,22 +38,13 @@ void print_number(int n)
 
 		tens = temp_tens / 10;
 
-
-
 		while (tens >= 1)
 
 		{
-
 			dig = pos / tens;
-
 			_putchar(dig + '0');
-
 			pos = (pos - (tens * dig));
-
 			tens /= 10;
-
 		}
-
 	}
-
 }
