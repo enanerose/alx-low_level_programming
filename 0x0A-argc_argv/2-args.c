5cfc260762fb28bb291e9506b32f41f3a1f16e7e
enanerose <enanerose97@gmail.com>
2022-07-24 13:13:23 -0700

    Argv Argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments receives
+ * @argc : argc contains the number of arguments passed to the program
+ * @argv : A vector is a one-dimensional array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for ( ; i < argc ; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
