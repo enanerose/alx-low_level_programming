3fadb5d199d9a95a3faf115f415a7e1014281271
enanerose <enanerose97@gmail.com>
2022-07-28 14:06:28 -0700

    another commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,17 +3,10 @@
 
 
 /**
-
  * _print - moves a string one place to the left and prints the string
-
  * @str: string to move
-
  * @l: size of string
-
- *
-
  * Return: void
-
  */
 
 void _print(char *str, int l)
@@ -23,24 +16,17 @@ void _print(char *str, int l)
 	int i, j;
 
 
-
 	i = j = 0;
-
 	while (i < l)
-
 	{
-
 		if (str[i] != '0')
 
 			j = 1;
-
 		if (j || i == l - 1)
-
 			_putchar(str[i]);
 
 		i++;
 	}
-
 	_putchar('\n');
 	free(str);
 }
@@ -61,44 +47,27 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
 
 	int j, k, mul, mulrem, add, addrem;
-
-
 	mulrem = addrem = 0;
-
 	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-
 	{
 
 		mul = (n - '0') * (num[j] - '0') + mulrem;
-
 		mulrem = mul / 10;
-
 		add = (dest[k] - '0') + (mul % 10) + addrem;
-
 		addrem = add / 10;
-
 		dest[k] = add % 10 + '0';
-
 	}
 
 	for (addrem += mulrem; k >= 0 && addrem; k--)
-
 	{
-
 		add = (dest[k] - '0') + addrem;
-
 		addrem = add / 10;
-
 		dest[k] = add % 10 + '0';
-
 	}
-
 	if (addrem)
-
 	{
 		return (NULL);
 	}
-
 	return (dest);
 }
 
