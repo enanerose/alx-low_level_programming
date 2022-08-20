978922e69e0e116d4334c541a85d7422763488e4
enanerose <enanerose97@gmail.com>
2022-08-20 12:37:56 -0700

    linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * __start - execute before main
+ * Description: execute before main
+ * Return: Nothing
+ */
+
+void __start(void) __attribute__((constructor));
+
+
+
+void __start(void)
+
+{
+
+printf("You're beat! and yet, you must allow,\n");
+
+printf("I bore my house upon my back!\n");
+
+}
