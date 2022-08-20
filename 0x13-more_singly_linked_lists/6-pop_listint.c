8fd29fc59d29e3a131286a8c8abc677febcd9a2d
enanerose <enanerose97@gmail.com>
2022-08-20 13:10:16 -0700

    more linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+
+
+/**
+ * pop_listint - deletes the head node of a list.
+ * @head: head of list
+ * Description: deletes the head node of a
+ * Return: head nodes data (n).
+ */
+
+int pop_listint(listint_t **head)
+
+{
+
+listint_t *temp = *head;
+
+int n;
+
+
+
+if (*head == NULL)
+
+return (0);
+
+
+
+n = (*head)->n;
+
+(*head) = (*head)->next;
+
+free(temp);
+
+return (n);
+
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,76 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - sum of all the data (n)
+ * @head: head of list
+ * @idx: index of node
+ * @n: integer
+ * Description: sum of all the data (n) of a listint_t linked list.
+ * Return: node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
+{
+
+listint_t *node = NULL;
+
+listint_t *actual = *head;
+
+unsigned int cont = 1;
+
+
+if (head == NULL)
+
+return (NULL);
+
+
+node = malloc(sizeof(listint_t));
+
+if (node == NULL)
+
+return (NULL);
+
+node->n = n;
+
+node->next = NULL;
+
+if (idx == 0)
+
+{
+
+node->next = *head;
+
+*head = node;
+
+return (node);
+
+}
+
+
+
+while (actual != NULL)
+
+{
+
+if (idx == cont)
+
+{
+
+node->next = actual->next;
+
+actual->next = node;
+
+return (node);
+
+}
+
+cont++;
+
+actual = actual->next;
+
+}
+
+return (NULL);
+
+}
